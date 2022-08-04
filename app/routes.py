042360b27a9a9686d49b24fc1ca910a5b383b2c3
Martin Gaucher <martin.gaucher@cotral.local>
2022-08-04 14:37:18 +0200

    Create template and base template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-
+    user = {'username':'Miguel'}
+    posts = [
+        {
+            'author' : {'username': 'John'},
+            'body' : 'Beautiful Day in Portland !'
+        },
+        {
+            'author' : {'username' : 'Susan'},
+            'body' : 'The avengers movie was so bad!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
